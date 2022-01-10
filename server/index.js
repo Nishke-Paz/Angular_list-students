0708894f387db4ea0f020cb106844af246e19dbf
Nishke-Paz <90559026+Nishke-Paz@users.noreply.github.com>
2022-01-10 13:37:16 +0300

    change server

--- server/index.js
+++ server/index.js
@@ -17,12 +17,6 @@ app.use(express.urlencoded());
 
 app.use(express.json());
 
-
-app.post('/search', function(request, response){
-  console.log("hello from server, your req = " + request.body.dish)
-  response.json({dish: `${new Date()}`})
-});
-
 app.get("/api", function (req, res) {
   res.status(200).json(st);
 });
